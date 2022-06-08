4601a9f9579403d9a6dc4229bb085fa18a374ebf
sairamesh1642 <sairamesh1642@gmail.com>
2022-06-08 18:38:22 +0530

    added about us

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <>
+            <div className="bg-[#ff620e] flex justify-between items-center" style={{ "fontFamily": "Yanone Kaffeesatz" }}>
+                <div className="shadow-xl m-20">
+
+                    <p className="text-6xl text-white text-center m-10 font-bold pt-10 tracking-wider">WHAT WE DO</p>
+                    <p className="pl-60 pr-60 pb-40 text-white font-thin tracking-wider text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolorum itaque voluptas ad voluptate quos, qui incidunt porro corporis quo placeat eum doloremque amet atque illum maiores laboriosam labore sapiente saepe repellendus ex minima nostrum. Magnam cumque vitae enim vel?</p>
+                </div>
+            </div>
+        </>
+    )
+}
