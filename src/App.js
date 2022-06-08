0e03f4865137179dff20cf54c805aca7aae06c20
sairamesh1642 <sairamesh1642@gmail.com>
2022-06-08 22:40:41 +0530

    added teams

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Teams from "./components/Teams";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<About />
+			<Teams />
 			Kuch bhi aise hi
 			<Footer />
 		</>
